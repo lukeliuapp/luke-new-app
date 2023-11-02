d7f1dbf7ecea3f9cbe053ba25d6bcb2c9a7880a5
LukeLiu <luke.liu@appscore.com.au>
2023-11-02 17:55:34 +1100

    Initial Commit

---
+++ LukeNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LukeNewsApp
+//
+//  Created by Luke Liu on 2/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
